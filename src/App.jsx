33fb51c65231e9cfaa0d53afff03577449b3547d
Muhammad Saad Naseem <msncoder1999@gmail.com>
2025-04-12 17:22:38 +0500

    complete project of password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [Password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
 
   const passowrdGenerator = useCallback(() => {
     let pass = "";
@@ -13,12 +15,22 @@ function App() {
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "`~!@#$%^&()-_=+[{]}|;:',<.>/?";
 
-    for (let i = 1; i < array.length; i++) {
-      let char = Math.floor(Math.rondom() * str.length + 1);
-      pass = str.charAt(char);
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordtoClipboard = useCallback(() => {
+    passwordRef.current.select();
+    window.navigator.clipboard.writeText(Password);
+  }, [Password]);
+
+  useEffect(() => {
+    passowrdGenerator();
+  }, [length, numberAllowed, charAllowed, passowrdGenerator]);
 
   return (
     <>
@@ -31,8 +43,12 @@ function App() {
             className="outline-none w-full py-1 px-3 bg-white text-orange-500"
             placeholder="password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="bg-blue-700 text-white px-3 py-0.5 shrink-0 ">
+          <button
+            onClick={copyPasswordtoClipboard}
+            className="bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer"
+          >
             Copy
           </button>
         </div>
@@ -47,7 +63,7 @@ function App() {
               className="
           cursor-pointer"
               onChange={(e) => {
-                setLength(e.target.value);
+                setLength(parseInt(e.target.value));
               }}
             />
             <label>Length: {length}</label>
@@ -71,7 +87,7 @@ function App() {
               defaultChecked={charAllowed}
               id="charAllowedInput"
               onChange={() => {
-                setNumberAllowed((prev) => !prev);
+                setCharAllowed((prev) => !prev);
               }}
             />
             <label htmlFor="charAllowedInput">char Allowed</label>
